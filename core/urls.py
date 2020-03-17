c6fdfb9e24aa52f0aa775bc72046dc9fb36740cd
emi <emazzurque@gmail.com>
2020-03-17 01:29:49 -0300

    agregado de contenido e modificacion de views.py

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+
 from core import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about', views.home, name="about"),
-    path('services', views.home, name="services"),
-    path('store', views.home, name="store"),
-    path('contact', views.home, name="contact"),
-    path('blog', views.home, name="blog"),
-    
+    path('about/', views.about, name="about"),
+    path('services/', views.services, name="services"),
+    path('store/', views.store, name="store"),
+    path('contact/', views.contact, name="contact"),
+    path('blog/', views.blog, name="blog"),
+    path('sample/', views.sample, name="sample"),
+
 ]
