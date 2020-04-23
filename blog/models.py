0e356eeea18ceb9beefd9cb84d22e8e54880b33a
emi <emazzurque@gmail.com>
2020-04-23 15:36:39 -0300

    aplicacion blog

--- blog/models.py
+++ blog/models.py
@@ -2,31 +2,28 @@ from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Category (models.Model):
-    title = models.CharField(max_length=200, verbose_name='Nombre')
-    created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creación')
-    update = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de actualizacióṇ̣')
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Nombre')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
 
     class Meta:
-        verbose_name = "categoría"
-        verbose_name_plural = "categorías"
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
         ordering = ['-created']
-    def __str__(self):
-        return self.title    
-    
-        
 
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Titulo')
-    published = models.DateTimeField(default = now, verbose_name='Fecha de publicación')
-    content = models.CharField(max_length=200, verbose_name='Contenido')
-    image = models.ImageField (verbose_name='Imagen', upload_to='services')
-    created = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de creación')
-    update = models.DateTimeField(auto_now_add=True,verbose_name='Fecha de actualizacióṇ̣')
-    categories =  models.ManyToManyField(Category, verbose_name='Categorias', related_name="get_posts")
-    author = models.ForeignKey(User, verbose_name='Autor', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, verbose_name='Título')
+    content = models.TextField(verbose_name='Contenido')
+    published = models.DateTimeField(verbose_name='Fecha de publicación', default=now)
+    image = models.ImageField(verbose_name="Imagen", upload_to="blog", null=True, blank=True)
+    author = models.ForeignKey(User, verbose_name="Autor", on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, verbose_name="Categorías", related_name="get_posts")
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
 
     class Meta:
         verbose_name = "Post"
@@ -34,9 +31,4 @@ class Post(models.Model):
         ordering = ['-created']
 
     def __str__(self):
-        return self.title    
-
-
-        
-    
-        
+        return self.title
