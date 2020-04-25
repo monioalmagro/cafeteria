d4bbdfa08a6761c28aee735a167438c5ff2fedd5
emi <emazzurque@gmail.com>
2020-04-25 01:01:54 -0300

    modifica urls generales

--- cafecito/urls.py
+++ cafecito/urls.py
@@ -1,17 +1,18 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 
-
 urlpatterns = [
-    path('',include('core.urls')),
+    path('', include('core.urls')),
+    path('services/', include('services.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls')),
-    path('sevices/',include('services.urls')),
     path('page/', include('pages.urls')),
 ]
-#aca mapeamos archivos estaticos si estamos debbugeando
+
+# aca mapeamos archivos estaticos si estamos debuggeando 
+
 if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
